6f22eb19441b88eb0b85494bae211027244f1d97
Xaver Hirsch <xaver.hirsch@livingpackets.com>
2020-06-22 13:48:42 +0200

    add react-ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import Bio from './components/Bio';
 import Images from './components/Images';
@@ -11,13 +12,18 @@ const StyledApp = styled.div`
   height: 100vh;
 `;
 
+ReactGA.initialize('UA-170290815-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 function App() {
   return (
     <StyledApp>
       <Router>
         <Switch>
           <Route exact path="/images" component={Images} />
-          <Route exact path="/"><Bio /></Route>
+          <Route exact path="/">
+            <Bio />
+          </Route>
         </Switch>
         <Nav />
       </Router>
